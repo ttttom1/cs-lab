b802598c199e237f0dc03e4da6bed7f4cb9cd549
SSAFY <>
2026-09-03 20:37:13 +0900

    fork execution script

--- linux-process-lab/01-fork/fork.c
+++ linux-process-lab/01-fork/fork.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <sys/wait.h>
 
 int main() {
-    printf("parent start pid=%%d\n", getpid());
+    printf("parent start pid=%d\n", getpid());
 
     pid_t pid =  fork();
 
