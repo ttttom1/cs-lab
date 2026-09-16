4f58aa8a426a45773d8d7d21bd4446371daec880
SSAFY <>
2026-09-16 10:50:41 +0900

    file_io.c 함수추출

--- mini-db/01-file-io/file_io.c
+++ mini-db/01-file-io/file_io.c
@@ -8,10 +8,44 @@
 
 #define PAGE_SIZE 4096
 
+void page_write(int fd, int page_id, const char *data) {
+    off_t offset = (off_t) page_id * PAGE_SIZE;
+
+    if(lseek(fd, offset,SEEK_SET) == -1) {
+        perror("lseek");
+        exit(1);
+    }
+
+    ssize_t written = write(fd, data, PAGE_SIZE);
+    
+    if (written != PAGE_SIZE) {
+        perror("write");
+        exit(1);
+    }
+
+    if (fsync(fd) == -1) {
+        perror("fsync");
+        exit(1);
+    }
+}
+
+void page_read(int fd, int page_id, char *buffer) {
+    off_t offset = (off_t) page_id * PAGE_SIZE;
+
+    if (lseek(fd, offset, SEEK_SET) == -1) {
+        perror("lseek");
+        exit(1);
+    } 
+
+    ssize_t bytes = read(fd,buffer,PAGE_SIZE);
+
+    if(bytes < 0) {
+        perror("read");
+        exit(1);
+    }
+}
+
 int main(void) {
-    //Todo1 
-    //database.db를 읽기/쓰기 가능하게 연다.
-    //파일이 없으면 생성한다.
     int fd = open("database.db", O_RDWR | O_CREAT,0644);
 
     if (fd < 0) {
@@ -20,44 +54,29 @@ int main(void) {
     }
 
     //page 하나 크기의 버퍼
-    char page[PAGE_SIZE];
+    char write_buffer[PAGE_SIZE];
+    memset(write_buffer, 0, PAGE_SIZE);
 
-    // 전체를 0으로 초기화
-    memset(page, 0, PAGE_SIZE);
+    strcpy(write_buffer, "Hello DB");
 
-    //TODO 2
-    //page 안에 "Hello DB" 저장
-    strcpy(page, "Hello DB");
-
-    //우리가 쓸 page
-    int page_id = 2;
-
-    //Todo 3
-    // page_id 를 실제파일 OFFSET으로 변환 
-    off_t offset = (off_t)page_id * PAGE_SIZE;
-
-    //todo4 :  lseek()를 이용해 page 2 위치로 이동
-    if (lseek(fd, offset, SEEK_SET) < 0) {
-        perror("lseek");
-        close(fd);
-        exit(1);
-    } 
-
-    // todo5 : write() 로 정확히 PAGE_SIZE 만큼 저장
-    if (write(fd, page, PAGE_SIZE) < 0) {
-        perror("write");
-        close(fd);
-        exit(1);
-    }
-
-    ///Todo 6
-    if (fsync(fd) < 0) {
-        perror("fsync");
-        close(fd);
-        exit(1);
-    }
+    page_write(fd, 2, write_buffer);
 
     close(fd);
-    printf("write complete\n");
+
+    fd = open("database.db",O_RDWR);
+    
+    if (fd < 0) {
+        perror("open");
+        exit(1);
+    }
+
+    char read_buffer[PAGE_SIZE];
+
+    memset(read_buffer,0,PAGE_SIZE);
+    page_read(fd, 2, read_buffer);
+
+    printf("read: %s\n", read_buffer);
+
+    close(fd);
     return 0;
 }
